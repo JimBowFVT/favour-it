9735c2cb37a6140ed00e8bd0a80dd8539e5dbf28
JimBowFVT <104732703+JimBowFVT@users.noreply.github.com>
2026-09-05 10:55:19 +0300

    chore: style community report moderation

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { supabase } from '../lib/supabase';
 import { assignCommunityGroupModerator, removeCommunityGroupModerator, listCommunityGroups, getCommunityGroupMembers } from '../lib/social';
 import './AdminPanel.css';
+import './AdminCommunityReports.css';
 
 const ADMIN_EMAIL = 'adamzoharlevi@gmail.com';
 
